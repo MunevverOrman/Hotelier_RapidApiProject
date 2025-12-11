e14f21e869373ea6628446b311eab5cbe54fd9b0
Münevver Orman <munevverorman1@gmail.com>
2025-12-11 23:20:05 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace HotelProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T t);
+
+        void Delete(T t);
+
+        void Update(T t);
+
+        T GetById(int id);
+
+        List<T> GetList();
+
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-I05VV5A;initial catalog =HotelDb;integrated security=true;");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+     
+    }
+}
